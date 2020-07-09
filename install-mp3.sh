a78295dffa3928f80e91a2e8492c5fa98cd5ea10
HSMM <56293541+HSMM@users.noreply.github.com>
2020-07-09 14:14:39 +0300

    Update install-mp3.sh

--- install-mp3.sh
+++ install-mp3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm -y
-yum install ffmpeg ffmpeg-devel -y
+yum install ffmpeg ffmpeg-devel mpg123 -y
 cd /var/lib/asterisk/agi-bin/
 wget https://raw.githubusercontent.com/HSMM/Freepbx-mp3-wav/master/realtime_transfer.sh
 chmod +x /var/lib/asterisk/agi-bin/realtime_transfer.sh
